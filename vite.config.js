1a4301984114bafc4d2b1462fb458bc588223c18
Ben Basil Tomy <benbasil00@gmail.com>
2023-04-22 13:09:04 +0530

    fifth build commit

--- vite.config.js
+++ vite.config.js
@@ -4,25 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build : {
-        chunkSizeWarningLimit: 1500,
-        rollupOptions: {
-          output: {
-            manualChunks: {
-              react: ['react', 'react-dom'],
-            },
-          },
-        },
-      },
 })
-// export default {
-//   build: {
-//     rollupOptions: {
-//       output: {
-//         manualChunks: {
-//           react: ['react', 'react-dom'],
-//         },
-//       },
-//     },
-//   },
-// }
